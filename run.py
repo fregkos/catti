abe1a59099de6689e9d1b283c4bcf91937e5cbd8
Periklis Fregkos <leajian13@gmail.com>
2021-09-26 17:39:59 +0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,14 @@
+# from catti.easterEgg import easterEgg
+from catti.menu import menu
+
+
+def main():
+    settings = {
+        'autoLoad': True
+    }
+    
+    # easterEgg()
+    menu(settings)
+
+if __name__ == "__main__":
+    main()
